abdb33b1fc27a5289069cd7a8cdc2bf3f48ecb27
zobla <zobla.kv@gmail.com>
2020-07-14 00:23:52 +0200

    added all filters in shop

--- public/js/shop.js
+++ public/js/shop.js
@@ -38,6 +38,8 @@ const selectedGenderFilters = [];
 
 const items = Array.from(products);
 
+items.forEach((e) => (e.style.display = "block"));
+
 // prettier-ignore
 for (let i = 0; i < brandFilters.length; i++)
   brandFilters[i].addEventListener("change", function () {
@@ -55,57 +57,104 @@ function removeBrandFilter(selectedBrandFilters, brandFilterKeywords,i){;
     const index = selectedBrandFilters.findIndex((e) => e === brandFilterKeywords[i]);
     selectedBrandFilters.splice(index, 1);
     displaySelectedBrands()
-    if(selectedBrandFilters.length === 0) displayAll()
+    if(selectedBrandFilters.length === 0) displayAll(selectedGenderFilters, 'gender')
 }
 
 function displaySelectedBrands() {
   items.forEach((e) => {
     const brand = e.children[1].innerHTML.split(" ")[0];
-    if (selectedBrandFilters.includes(brand)) e.style.display = "block";
+    const gender = e.children[1].innerHTML.substr(
+      e.children[1].innerHTML.length - 1
+    );
+    if (selectedBrandFilters.includes(brand))
+      if (selectedGenderFilters.length > 0) {
+        if (selectedGenderFilters.includes(gender)) e.style.display = "block";
+      } else {
+        e.style.display = "block";
+      }
     else e.style.display = "none";
   });
 }
 
-function displayAll() {
-  items.forEach((e) => (e.style.display = "block"));
-}
-
-for (let i = 0; i < genderFilters.length; i++)
-  genderFilters[i].addEventListener("change", function () {
-    if (this.checked)
-      applyGenderFilter(selectedGenderFilters, genderFilterKeywords, i);
-    else removeGenderFilter(selectedGenderFilters, genderFilterKeywords, i);
-  });
-
 // prettier-ignore
-function applyGenderFilter(selectedGenderFilters, genderFilterKeywords,i) {
-  selectedGenderFilters.push(genderFilterKeywords[i]);
-  displaySelectedGenders()
-}
-
-function removeGenderFilter(selectedGenderFilters, genderFilterKeywords, i) {
-  const index = selectedGenderFilters.findIndex(
-    (e, i) => e === genderFilterKeywords[i]
-  );
-  selectedGenderFilters.splice(index, 1);
-  displaySelectedGenders();
-  if (selectedGenderFilters.length === 0) displayAll();
-}
-
-// prettier-ignore
-function displaySelectedGenders() {
-  items.forEach((e) => {
-    const gender = e.children[1].innerHTML.substr(
-      e.children[1].innerHTML.length - 1
-    );
-    if (e.style.display === "block" && !selectedGenderFilters.includes(gender)) {
-      e.style.display = "none";
-      console.log("fired");
+function displayAll(selectedFilter, type) {
+  if(selectedFilter.length === 0) items.forEach(e => e.style.display = 'block')
+  else {
+    if(type === 'brand'){
+      items.forEach((e) => {
+        const brand = e.children[1].innerHTML.split(" ")[0]
+        if(e.style.display === 'none' && selectedFilter.includes(brand)) 
+          e.style.display = 'block';
+        else e.style.display = 'none'
+      })
     }
-  });
+    else {
+      items.forEach((e) => {
+        const gender = e.children[1].innerHTML.substr(e.children[1].innerHTML.length - 1)
+        if(e.style.display === 'none' && selectedFilter.includes(gender)) {
+          e.style.display = 'block';
+        }
+        else e.style.display = 'none'
+      })
+    }
+  }
 }
 
 // prettier-ignore
+for(let i=0;i<genderFilters.length;i++)
+genderFilters[i].addEventListener('change', function(){
+  if(this.checked){
+    selectedGenderFilters.push(genderFilterKeywords[i]);
+    items.forEach(e => {
+      const gender = e.children[1].innerHTML.substr(e.children[1].innerHTML.length - 1)
+      const brand = e.children[1].innerHTML.split(" ")[0];
+      if (selectedGenderFilters.includes(gender))
+        if (selectedBrandFilters.length > 0) {
+          if (selectedBrandFilters.includes(brand)) e.style.display = "block"; 
+        } else {
+          e.style.display = "block";
+        }
+      else {
+        e.style.display = "none";
+      }
+    })
+  }
+  else{
+    const index = selectedGenderFilters.findIndex((e) => e === genderFilterKeywords[i]);
+    selectedGenderFilters.splice(index, 1);
+    items.forEach(e => {
+      const gender = e.children[1].innerHTML.substr(e.children[1].innerHTML.length - 1)
+      if(e.style.display === 'block' && selectedGenderFilters.includes(gender)) return
+      else e.style.display = 'none';
+    })
+  }
+  if(selectedGenderFilters.length === 0) displayAll(selectedBrandFilters, 'brand')
+})
+
+// sort
+const shopItemsContainer = document.getElementById("shopItemsContainer");
+const latestFilter = document.getElementById("latestFilter");
+const priceFilter = document.getElementById("priceFilter");
+const regularSortedItems = [...items];
+
+priceFilter.addEventListener("change", function () {
+  if (this.checked) {
+    items.sort(function (a, b) {
+      return (
+        Number(b.children[2].innerHTML.split(" ")[0]) -
+        Number(a.children[2].innerHTML.split(" ")[0])
+      );
+    });
+    for (let i = 0; i < items.length; i++)
+      shopItemsContainer.appendChild(items[i]);
+  }
+});
+
+latestFilter.addEventListener("change", function () {
+  if (this.checked)
+    for (let i = 0; i < regularSortedItems.length; i++)
+      shopItemsContainer.appendChild(regularSortedItems[i]);
+});
 
 clearSearch.addEventListener("click", () => {
   if (searchInput.value !== "") {
